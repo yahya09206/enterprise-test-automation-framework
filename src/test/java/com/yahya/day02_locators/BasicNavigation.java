56fc1717871122dbbdecafb5f60599d24a2b21cd
yahya09206 <yahyahussein91@gmail.com>
2026-02-23 11:45:33 -0800

    added a wait and quit to browser

--- src/test/java/com/yahya/day02_locators/BasicNavigation.java
+++ src/test/java/com/yahya/day02_locators/BasicNavigation.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // Setup browser driver
         WebDriverManager.chromedriver().setup();
@@ -15,5 +15,7 @@ public class BasicNavigation {
         // navigate to website
         driver.get("https://www.tesla.com");
 
+        Thread.sleep(2000);
+        driver.quit();
     }
 }
