a3babbb9d7441687101eabcde15ac84e162626c5
yahya09206 <yahyahussein91@gmail.com>
2026-02-25 13:10:50 -0800

    first validation for remember me

---
+++ src/test/java/com/yahya/day03_css_selector_x_path/ValidateCRM.java
@@ -0,0 +1,27 @@
+package com.yahya.day03_css_selector_x_path;
+
+import com.yahya.util.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ValidateCRM {
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        //driver.navigate().to("https://login1.nextbasecrm.com/");
+        driver.get("https://login1.nextbasecrm.com/");
+
+        WebElement rememberText = driver.findElement(By.className("login-item-checkbox-label"));
+
+        String expectedText = "Remember me on this computer";
+        String actualText = rememberText.getText();
+
+        if (actualText.equals(expectedText)){
+            System.out.println("Remember me validation passed");
+        } else {
+            System.out.println("Remember me validation failed");
+        }
+
+    }
+}
