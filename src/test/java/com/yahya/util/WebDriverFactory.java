1d47ec1c40d35c55c9441113aaed362311b31907
yahya09206 <yahyahussein91@gmail.com>
2026-02-25 12:23:20 -0800

    setting up webdriver factory

---
+++ src/test/java/com/yahya/util/WebDriverFactory.java
@@ -0,0 +1,25 @@
+package com.yahya.util;
+
+import org.openqa.selenium.WebDriver;
+
+public class WebDriverFactory {
+
+
+    /**
+     * Create a new method called getDriver
+     * static method
+     * accepts string arg: browserType
+     *  - This arg will determine what type of browser is opened
+     *  - if "chrome" is passed --> it will open Chrome browser
+     *  - if "firefox" is passed --> it will open firefox browser
+     *  - Return Type --> WebDriver
+     */
+
+    public static WebDriver getDriver(String browserType){
+
+        switch(browserType):
+        case 1:
+
+
+    }
+}
