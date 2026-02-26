330fdcdebb090f1f9ad17177db2677148b0b1fce
yahya09206 <yahyahussein91@gmail.com>
2026-02-26 12:36:32 -0800

    multiple elements validated by links

---
+++ src/test/java/com/yahya/day04_find_elements_radio_button/FindElements.java
@@ -0,0 +1,30 @@
+package com.yahya.day04_find_elements_radio_button;
+
+import com.yahya.util.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.time.Duration;
+import java.util.List;
+
+public class FindElements {
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://practice.cydeo.com/abtest");
+
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+
+        for (WebElement allLink : allLinks) {
+            System.out.println(allLink.getSize());
+            System.out.println(allLink.getText());
+            System.out.println(allLink.getAttribute("href"));
+        }
+
+        driver.quit();
+
+    }
+}
