f91ae9c6657f0ed53bb1b560d889b04ec4065360
yahya09206 <yahyahussein91@gmail.com>
2026-02-28 09:37:13 -0800

    added driver and implicit wait

--- src/test/java/com/yahya/day05_dynamic_elements_testng_dropdowns/AddRemoveElements.java
+++ src/test/java/com/yahya/day05_dynamic_elements_testng_dropdowns/AddRemoveElements.java
@@ -3,9 +3,13 @@ package com.yahya.day05_dynamic_elements_testng_dropdowns;
 import com.yahya.util.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 
+import java.time.Duration;
+
 public class AddRemoveElements {
     public static void main(String[] args) {
 
         WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("https://practice.cydeo.com/add_remove_elements/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 }
