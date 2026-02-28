e998eb5e69b6079e0432909e2b38be0091a1beef
yahya09206 <yahyahussein91@gmail.com>
2026-02-28 09:35:56 -0800

    setting up validation for stale elements

---
+++ src/test/java/com/yahya/day05_dynamic_elements_testng_dropdowns/AddRemoveElements.java
@@ -0,0 +1,11 @@
+package com.yahya.day05_dynamic_elements_testng_dropdowns;
+
+import com.yahya.util.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+public class AddRemoveElements {
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+    }
+}
