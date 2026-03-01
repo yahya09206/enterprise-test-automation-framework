2d3fc9420d12ca7799b93a98543bc1e3f9a466d4
yahya09206 <yahyahussein91@gmail.com>
2026-03-01 10:46:01 -0800

    generated method for verifying radio buttons

--- src/test/java/com/yahya/day05_dynamic_elements_testng_dropdowns/RadioUtility.java
+++ src/test/java/com/yahya/day05_dynamic_elements_testng_dropdowns/RadioUtility.java
@@ -15,19 +15,22 @@ public class RadioUtility {
         driver.get("https://practice.cydeo.com/radio_buttons");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        List<WebElement> radioBtns = driver.findElements(By.xpath("//input[@name='sport']"));
+    }
+
+    private static void clickAndVerifyRadioButton(WebDriver driver, String nameAttr, String expectedID){
+
+        List<WebElement> radioBtns = driver.findElements(By.name(nameAttr));
 
         for (WebElement radioBtn : radioBtns) {
 
             String actualID = radioBtn.getAttribute("id");
             System.out.println(actualID);
 
-            if (actualID.equals("hockey")){
+            if (actualID.equals(expectedID)){
                 radioBtn.click();
-                System.out.println(radioBtn.isSelected());
+                System.out.println(actualID + "is Selected: " + radioBtn.isSelected());
                 break;
             }
         }
-
     }
 }
