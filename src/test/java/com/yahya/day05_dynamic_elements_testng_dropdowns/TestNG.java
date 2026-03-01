8d39424a2d0a35c50eb38aad5b55d8bac61dae01
yahya09206 <yahyahussein91@gmail.com>
2026-03-01 11:00:58 -0800

    trying before method

--- src/test/java/com/yahya/day05_dynamic_elements_testng_dropdowns/TestNG.java
+++ src/test/java/com/yahya/day05_dynamic_elements_testng_dropdowns/TestNG.java
@@ -1,4 +1,21 @@
 package com.yahya.day05_dynamic_elements_testng_dropdowns;
 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class TestNG {
+
+    @BeforeMethod
+    public void setupMethod(){
+        System.out.println("--> Before method is running");
+    }
+    @Test
+    public void test1(){
+        System.out.println("Test 1 is running");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test 2 is running");
+    }
 }
