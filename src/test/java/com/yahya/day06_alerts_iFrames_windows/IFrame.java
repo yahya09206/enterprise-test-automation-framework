e8f55efdcb22c8c5b715739ea3307e27ad1655a2
yahya09206 <yahyahussein91@gmail.com>
2026-03-04 12:37:19 -0800

    validated iframe

--- src/test/java/com/yahya/day06_alerts_iFrames_windows/IFrame.java
+++ src/test/java/com/yahya/day06_alerts_iFrames_windows/IFrame.java
@@ -4,6 +4,7 @@ import com.yahya.util.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -30,7 +31,12 @@ public class IFrame {
 
         driver.switchTo().frame("mce_0_ifr");
         WebElement paragraphText = driver.findElement(By.xpath("//p[.='Your content goes here.']"));
-        paragraphText.isDisplayed();
+        Assert.assertTrue(paragraphText.isDisplayed());
+        Thread.sleep(2000);
+
+        driver.switchTo().defaultContent();
+        WebElement detailText = driver.findElement(By.tagName("h3"));
+        Assert.assertTrue(detailText.isDisplayed());
         Thread.sleep(2000);
 
     }
