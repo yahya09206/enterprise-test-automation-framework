57feb4256ad367a91e14e1470ca9f2b364c0c05a
yahya09206 <yahyahussein91@gmail.com>
2026-03-04 12:32:04 -0800

    validating text in iframe

---
+++ src/test/java/com/yahya/day06_alerts_iFrames_windows/IFrame.java
@@ -0,0 +1,39 @@
+package com.yahya.day06_alerts_iFrames_windows;
+
+import com.yahya.util.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class IFrame {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup(){
+        driver = WebDriverFactory.getDriver("chrome");
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterClass
+    public void teardown(){
+        driver.quit();
+    }
+
+    @Test
+    public void verify_iFrame() throws InterruptedException {
+
+        driver.get("https://practice.cydeo.com/iframe");
+
+        driver.switchTo().frame("mce_0_ifr");
+        WebElement paragraphText = driver.findElement(By.xpath("//p[.='Your content goes here.']"));
+        paragraphText.isDisplayed();
+        Thread.sleep(2000);
+
+    }
+
+
+}
