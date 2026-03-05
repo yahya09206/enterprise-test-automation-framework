18e34a5075ad3ef45762b776ab6b65875ab9cf8e
yahya09206 <yahyahussein91@gmail.com>
2026-03-05 11:26:27 -0800

    setting up properties to read from file

---
+++ src/test/java/com/yahya/day08_properties_configreader/ReadingFromProperties.java
@@ -0,0 +1,22 @@
+package com.yahya.day08_properties_configreader;
+
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadingFromProperties {
+
+    @Test
+    public void readFromProperties() throws IOException {
+
+        Properties properties = new Properties();
+
+        FileInputStream file = new FileInputStream("config.properties");
+
+        properties.load(file);
+
+
+    }
+}
