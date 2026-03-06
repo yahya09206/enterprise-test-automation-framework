b6be0ac2a310c2ab45768705d2dbf8726524ca50
yahya09206 <yahyahussein91@gmail.com>
2026-03-06 10:13:07 -0800

    setting up driver using singleton

---
+++ src/test/java/com/yahya/util/Driver.java
@@ -0,0 +1,17 @@
+package com.yahya.util;
+
+import org.openqa.selenium.WebDriver;
+
+public class Driver {
+
+    // Private constructor to remove access to object
+
+    private Driver(){
+    }
+
+    /**
+     * Driver is private so no access from outside of class
+     */
+
+    private static WebDriver driver;
+}
