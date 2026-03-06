cd3aed5e6bc760799211f4f0576cd07283c30e23
yahya09206 <yahyahussein91@gmail.com>
2026-03-06 10:19:12 -0800

    set up driver for chrome

--- src/test/java/com/yahya/util/Driver.java
+++ src/test/java/com/yahya/util/Driver.java
@@ -1,6 +1,10 @@
 package com.yahya.util;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class Driver {
 
@@ -11,7 +15,35 @@ public class Driver {
 
     /**
      * Driver is private so no access from outside of class
+     * Made it static so it can be used in a static method
      */
 
     private static WebDriver driver;
+
+
+    public static WebDriver getDriver(){
+
+        // check if driver is null
+        if (driver == null){
+
+            /**
+             * read browser type from config file so it doesn't get affected from any changes
+             */
+            String browserType = ConfigReader.getProperty("browser");
+
+            switch (browserType){
+
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            }
+
+        }
+
+        // return existing driver if it is not null
+        return driver;
+    }
+
+
 }
